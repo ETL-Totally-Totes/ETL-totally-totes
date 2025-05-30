53e9b7ba27c526d2bda6fdfd19ba9f343775ff23
SelvitaPR <selva.pla.roj@gmail.com>
2025-05-30 09:53:56 +0100

    create s3 buckets

---
+++ terraform/s3.tf
@@ -0,0 +1,29 @@
+resource "aws_s3_bucket" "code_bucket" {
+  bucket = var.code_bucket_name
+
+  tags = {
+    Name        = "Source code"
+    Environment = "prod"
+  }
+}
+
+resource "aws_s3_bucket" "ingestion_bucket" {
+  bucket = var.ingestion_bucket_name
+
+  tags = {
+    Name        = "Data ingested"
+    Environment = "prod"
+  }
+}
+
+resource "aws_s3_object" "extract_function" {
+  bucket = var.code_bucket_name
+  key    = "extract_function.zip"
+  source = "${path.module}/../extract_function.zip"
+}
+
+resource "aws_s3_object" "layer" {
+  bucket = var.code_bucket_name
+  key    = "layer.zip"
+  source = "${path.module}/../layer.zip"
+}
