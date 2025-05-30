c692f53e379a6f30e9755898c87b9e94278fb769
SelvitaPR <selva.pla.roj@gmail.com>
2025-05-30 09:53:26 +0100

    create tf provider and backend

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+    Project_name = "ETL-totally-totes",
+    terraform_managed = "true",
+    repo_link = "https://github.com/ETL-Totally-Totes/ETL-totally-totes"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "etl-totally-totes-backend"
+    key    = "terraform/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
