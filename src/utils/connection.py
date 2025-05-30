0bfb5761484ab41bbca04da61a57350578e5fd31
AbbyAdj <abbyadjei9@gmail.com>
2025-05-30 11:10:50 +0100

    add cursor for errors from pandas

--- src/utils/connection.py
+++ src/utils/connection.py
@@ -1,23 +1,24 @@
 import psycopg2 
+from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv(override=True)
+
+database = os.environ["PG_DATABASE"]
+username = os.environ["PG_USERNAME"]
+password = os.environ["PG_PASSWORD"]
+host = os.environ["PG_HOST"]
 
 def create_connection():
-
-    database = os.environ["PG_DATABASE"]
-    username = os.environ["PG_USERNAME"]
-    password = os.environ["PG_PASSWORD"]
-    host = os.environ["PG_HOST"]
-
     db = psycopg2.connect(
-            database="totesys",
-            user="project_team_06",
-            password="fZUyorR8LdL8uBU",
-            host="nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com",
+            database=database,
+            user=username,
+            password=password,
+            host=host,
+            cursor_factory=RealDictCursor
         )
-    return db
+    return db.cursor()
 
 def close_connection(db):
     db.close()
