379999309b468d7568597f24e35667f8c6baee67
SelvitaPR <selva.pla.roj@gmail.com>
2025-06-02 12:08:18 +0100

    vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,19 +1,19 @@
-# variable "ingestion_bucket_name" {
-#   type    = string
-#   default = "totally-totes-ingestion"
-# }
+variable "ingestion_bucket_name" {
+  type    = string
+  default = "totally-totes-ingestion"
+}
 
-# variable "code_bucket_name" {
-#   type    = string
-#   default = "totally-totes-code-src"
-# }
+variable "code_bucket_name" {
+  type    = string
+  default = "totally-totes-code-src"
+}
 
-# variable "extract_lambda_name" {
-#   type    = string
-#   default = "extract_handler"
-# }
+variable "extract_lambda_name" {
+  type    = string
+  default = "extract_handler"
+}
 
-# variable "python_runtime" {
-#   type    = string
-#   default = "python3.12"
-# }
+variable "python_runtime" {
+  type    = string
+  default = "python3.12"
+}
