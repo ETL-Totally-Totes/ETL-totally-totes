ae583decd33835104ab84870bf412691c5e27e78
SelvitaPR <selva.pla.roj@gmail.com>
2025-06-02 12:07:48 +0100

    lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,39 +1,39 @@
-# data "archive_file" "extract_lambda" {
-#   type             = "zip"
-#   output_file_mode = "0666"
-#   source_file      = "${path.module}/../src/extract.py"
-#   output_path      = "${path.module}/../extract_function.zip"
-# }
+data "archive_file" "extract_lambda" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_file      = "${path.module}/../src/extract.py"
+  output_path      = "${path.module}/../extract_function.zip"
+}
 
-# data "archive_file" "layer" {
-#   type             = "zip"
-#   output_file_mode = "0666"
-#   source_dir       = "${path.module}/../layer/"
-#   output_path      = "${path.module}/../layer.zip"
-# }
+data "archive_file" "layer" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_dir       = "${path.module}/../layer/"
+  output_path      = "${path.module}/../layer.zip"
+}
 
-# resource "aws_lambda_layer_version" "etl_layer" {
-#   layer_name          = "etl_layer"
-#   compatible_runtimes = [var.python_runtime]
-#   s3_bucket = aws_s3_bucket.code_bucket.id
-#   s3_key = "layer.zip"
-# }
+resource "aws_lambda_layer_version" "etl_layer" {
+  layer_name          = "etl_layer"
+  compatible_runtimes = [var.python_runtime]
+  s3_bucket = aws_s3_bucket.code_bucket.id
+  s3_key = "layer.zip"
+}
 
-# resource "aws_lambda_function" "extract_handler" {
-#   function_name = var.extract_lambda_name
-#   role          = aws_iam_role.lambda_role.arn
-#   handler       = "extract.saffi"
-#   s3_bucket = aws_s3_bucket.code_bucket.id
-#   s3_key = "extract_function.zip"
+resource "aws_lambda_function" "extract_handler" {
+  function_name = var.extract_lambda_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "extract.saffi"
+  s3_bucket = aws_s3_bucket.code_bucket.id
+  s3_key = "extract_function.zip"
 
-#   source_code_hash = data.archive_file.extract_lambda.output_base64sha256
-#   timeout = 10
+  source_code_hash = data.archive_file.extract_lambda.output_base64sha256
+  timeout = 10
 
-#   runtime = "python3.13"
-#   layers = [aws_lambda_layer_version.etl_layer.arn]
-#   environment {
-#     variables = {
-#       S3_BUCKET_NAME = var.code_bucket_name
-#     }
-#   }
-# }
+  runtime = "python3.13"
+  layers = [aws_lambda_layer_version.etl_layer.arn]
+  environment {
+    variables = {
+      S3_BUCKET_NAME = var.code_bucket_name
+    }
+  }
+}
