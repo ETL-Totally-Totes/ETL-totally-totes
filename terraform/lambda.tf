b09393d9ba5b0b3ceb3fa03462d3fe8e77ef0777
SelvitaPR <selva.pla.roj@gmail.com>
2025-06-02 14:42:24 +0100

    lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -12,11 +12,26 @@ data "archive_file" "layer" {
   output_path      = "${path.module}/../layer.zip"
 }
 
+data "archive_file" "utils" {
+  type             = "zip"
+  output_file_mode = "0666"
+  source_file       = "${path.module}/../src/utils/connection.py"
+  output_path      = "${path.module}/../utils/python/src/utils.zip"
+}
+
+
 resource "aws_lambda_layer_version" "etl_layer" {
   layer_name          = "etl_layer"
   compatible_runtimes = [var.python_runtime]
   s3_bucket = aws_s3_bucket.code_bucket.id
   s3_key = "layer.zip"
+}
+
+resource "aws_lambda_layer_version" "utils" {
+  layer_name          = "utils"
+  compatible_runtimes = [var.python_runtime]
+  s3_bucket = aws_s3_bucket.code_bucket.id
+  s3_key = "utils.zip"
 }
 
 resource "aws_lambda_function" "extract_handler" {
@@ -30,7 +45,7 @@ resource "aws_lambda_function" "extract_handler" {
   timeout = 10
 
   runtime = "python3.13"
-  layers = [aws_lambda_layer_version.etl_layer.arn]
+  layers = [aws_lambda_layer_version.etl_layer.arn, aws_lambda_layer_version.utils.arn]
   environment {
     variables = {
       S3_BUCKET_NAME = var.code_bucket_name
