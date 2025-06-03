c3d519ee000eb8612dc89470819c6da90ccd2ea7
SelvitaPR <selva.pla.roj@gmail.com>
2025-06-03 10:55:47 +0100

    iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,31 +1,25 @@
-# # ---------------
-# # Lambda IAM Role
-# # ---------------
-
-# Define
+# ---------------
+# Lambda IAM Role
+# ---------------
+# Define
 data "aws_iam_policy_document" "trust_policy" {
   statement {
     effect = "Allow"
-
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
-
     actions = ["sts:AssumeRole"]
   }
 }
-
-# # Create
+# Create
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
   assume_role_policy = data.aws_iam_policy_document.trust_policy.json
 }
-
 # ------------------------------
 # Lambda IAM Policy for S3
 # ------------------------------
-
 # Define
 data "aws_iam_policy_document" "s3_data_policy_doc" {
   statement {
@@ -37,24 +31,19 @@ data "aws_iam_policy_document" "s3_data_policy_doc" {
     ]
   }
 } #TODO: ADD MORE BUCKETS WHEN WE CREATE THEM
-
 # Create
 resource "aws_iam_policy" "lambda_s3_policy" {
   name = "lambda_s3_policy"
-  policy      = data.aws_iam_policy_document.s3_data_policy_doc.json 
+  policy      = data.aws_iam_policy_document.s3_data_policy_doc.json
 }
-
 # Attach
 resource "aws_iam_role_policy_attachment" "lambda_s3_write_policy_attachment" {
     role = aws_iam_role.lambda_role.name #TODO: attach the s3 write policy to the lambda role
     policy_arn =  aws_iam_policy.lambda_s3_policy.arn
-
 }
-
 # ------------------------------
 # Lambda IAM Policy for CloudWatch
 # ------------------------------
-
 # Define
 data "aws_iam_policy_document" "cw_document" {
   statement {
@@ -64,7 +53,6 @@ data "aws_iam_policy_document" "cw_document" {
       ]
     resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
   }
-
   statement {
     #this statement should give permission to create Log Streams and put Log Events in the lambda's own Log Group
     effect = "Allow"
@@ -76,27 +64,21 @@ data "aws_iam_policy_document" "cw_document" {
     resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
   }
 }
-
 # Create
 resource "aws_iam_policy" "cw_policy" {
   #TODO: use the policy document defined above
   policy      = data.aws_iam_policy_document.cw_document.json
 }
-
-# Attach
+# Attach
 resource "aws_iam_role_policy_attachment" "lambda_cw_policy_attachment" {
   #TODO: attach the cw policy to the lambda role
-  role = aws_iam_role.lambda_role.name 
+  role = aws_iam_role.lambda_role.name
   policy_arn =  aws_iam_policy.cw_policy.arn
 }
-
-
 # ------------------------------
 # Lambda IAM Policy for SNS
 # ------------------------------
-
 # Define
-
 data "aws_iam_policy_document" "sns_topic_document" {
   statement {
     actions = [
@@ -110,36 +92,69 @@ data "aws_iam_policy_document" "sns_topic_document" {
       "SNS:DeleteTopic",
       "SNS:AddPermission",
     ]
-
     # condition {
     #   test     = "StringEquals"
     #   variable = "AWS:SourceOwner"
-
     #   values = [
     #     var.account-id,
     #   ]
     # }
-
     effect = "Allow"
-
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
     resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
-
   }
 }
-
 # Create
 resource "aws_iam_policy" "sns_topic_policy" {
   #TODO: use the policy document defined above
   policy      = data.aws_iam_policy_document.sns_topic_document.json
   #data.aws_iam_policy_document.sns_topic_document.json
 }
-# Attach
+# Attach
 resource "aws_iam_role_policy_attachment" "lambda_sns_topic_policy_attachment" {
   #TODO: attach the cw policy to the lambda role
-  role = aws_iam_role.lambda_role.name 
+  role = aws_iam_role.lambda_role.name
   policy_arn =  aws_iam_policy.cw_policy.arn
 }
-
 data "aws_caller_identity" "current" {}
-
 data "aws_region" "current" {}
+#-----------------
+# state machine
+#-----------------
+data "aws_iam_policy_document" "state_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["states.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+resource "aws_iam_role" "state_role" {
+  name = "state_role"
+  assume_role_policy = data.aws_iam_policy_document.state_policy.json
+}
+data "aws_iam_policy_document" "state_machine_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+    resources = ["${aws_lambda_function.extract_lamba_handler.arn}:*"]
+  }
+}
+#TODO:add other lambdas
+# Create
+resource "aws_iam_policy" "state_machine_role_policy" {
+  name = "state_machine_role_policy"
+  policy      = data.aws_iam_policy_document.state_machine_role_policy.json
+}
+# Attach
+resource "aws_iam_role_policy_attachment" "state_machine_attachment" {
+    role = aws_iam_role.lambda_role.name #TODO: attach the s3 write policy to the lambda role
+    policy_arn =  aws_iam_policy.state_machine_role_policy.arn
+}
