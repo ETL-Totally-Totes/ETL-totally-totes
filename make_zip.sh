d016223deec937bd59029e93315c984fec7a616b
AbbyAdj <abbyadjei9@gmail.com>
2025-06-03 14:48:10 +0100

    script for making zip files

---
+++ make_zip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Cleaning up..."
+rm -rf python 
+rm -rf util_layer.zip
+rm -rf layer.zip
+echo "Creating dependencies"
+pip install -r requirements.txt --platform manylinux2014_x86_64 --only-binary=:all: -t python/
+zip -r layer.zip python
+rm -rf python
+echo "Creating utils"
+mkdir python
+cp -r src python
+zip -r util_layer.zip python
+cd terraform
