60572287e8b20b08a643afa5d0a3211528734bcc
SelvitaPR <selva.pla.roj@gmail.com>
2025-06-05 11:56:07 +0100

    write first test for util function

--- tests/test_transform/test_transform_utils.py
+++ tests/test_transform/test_transform_utils.py
@@ -1,49 +1,39 @@
 from moto import mock_aws
+from unittest.mock import patch, Mock, MagicMock
 from src.utils.utils import read_csv_to_df
 import pytest
 import boto3
 import pandas as pd
-import os
 import tempfile
+from io import BytesIO
 
+#things to mock: 
+#1. get_csv_file_keys
+#2. s3_client
+#3. s3_client.get_object
 
-# test that you give a csv and get a dataframe
-# test it is taking from bucket
-
-@pytest.fixture
-def s3_bucket_setup(s3_client):
-    with mock_aws():
-        bucket_name = "test_bucket"
-        object = "test_object.csv"
-        bucket = s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": "eu-west-2"})
-
-# @pytest.fixture
-# def put_csv_in_bucket(s3_bucket_setup, s3_client):
-#         body = """address_id,address_line_1,address_line_2,district	city,postal_code,country,phone,created_at,last_updated\n
-#                 1,6826 Herzog Via,Avon,New Patienceburgh,28441,Turkey,1803 637401,20:50.0,20:50.0"""
-#         put_object = s3_client.put_object(Bucket=bucket, Key=object, Body=body)
-#         get_object = s3_client.get_object(Bucket=bucket, Key=object)
-#         response = get_object['Body'].read().decode('utf-8')
-#         return response
-
+@mock_aws
 class TestReadCsvToDf:
-    # def test_access_to_s3(self, s3_boto):
+    TEST_BUCKET = 'test_ingestion_bucket'
+    CSV_1 = "address_id,address_line_1,address_line_2\n1,6826 Herzog Via,Avon"
+    CSV_2 = "address2_id,address2_line_1,address_line2_2\n1,12 Gold Close,Harlow"
 
-    def test_func_returns_df_from_list_of_one_key(self, s3_with_bucket):
-        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tmp:
-            body = "address_id,address_line_1,address_line_2,district city,postal_code,country,phone,created_at,last_updated \n1,6826 Herzog Via,Avon,New Patienceburgh,28441,Turkey,1803 637401,20:50.0,20:50.0"
-            tmp.write(body)
-            tmp_path = tmp.name
-            file_name = os.path.basename(tmp_path)
-            print(file_name)
-
-        with mock_aws():
-            s3_with_bucket.return_value = file_name
-
-        test_input = ['payment_2025-06-04 10:56:19.770508+00:00.csv']
-        result = read_csv_to_df(test_input) #returns a dataframe
-        print(type(result))
-        # expected_df = pd.DataFrame(body) #string of expected df
-        # print(expected_df)
-        # assert result == expected_df
+    def test_single_key_list_yields_single_df(self, aws_credentials):
+        s3_client = boto3.client('s3', region_name="eu-west-2")
+        s3_client.create_bucket(
+        Bucket=self.TEST_BUCKET,
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+    )
+        s3_client.put_object(Bucket=self.TEST_BUCKET, Key="test_object_1.csv", Body=self.CSV_1)
+        s3_client.put_object(Bucket=self.TEST_BUCKET, Key="test_object_2.csv", Body=self.CSV_2)
+        test_list = ['test_object_1.csv']
+        result = read_csv_to_df(test_list, s3_client, self.TEST_BUCKET)
+        expected_df = pd.DataFrame({'address_id':[1],'address_line_1':['6826 Herzog Via'],'address_line_2':['Avon']})
+        expected = {test_list[0]:expected_df}
+        final = next(result)
+        compare_dfs : pd.DataFrame = final[test_list[0]].compare(expected_df)
+        
+        assert compare_dfs.shape[0] == 0
+        assert compare_dfs.shape == (0,0)
+        
     
