7d869ceacfa5fc2e90bd903ae4414b2f1ebf3452
SelvitaPR <selva.pla.roj@gmail.com>
2025-06-06 17:51:21 +0100

    add transform_handler variables

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -17,3 +17,17 @@ variable "python_runtime" {
   type    = string
   default = "python3.12"
 }
+
+# ------------------------------
+# Tranform lambda tf code
+# ------------------------------
+
+variable "transformation_bucket_name" {
+  type    = string
+  default = "our-totally-totes-processed"
+}
+
+variable "transform_lambda_name" {
+  type    = string
+  default = "transform_handler"
+}
