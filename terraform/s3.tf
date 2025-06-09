605ed489a63f5f19d80cd7e11ccdf81262fe3e46
SelvitaPR <selva.pla.roj@gmail.com>
2025-06-09 15:07:23 +0100

    create s3 object for transform lambda

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -24,13 +24,21 @@ resource "aws_s3_object" "extract_function" {
   etag   = filemd5(data.archive_file.extract_lambda.output_path)
 }
 
-resource "aws_s3_object" "layer" {
-  depends_on = [ aws_s3_bucket.code_bucket ]
-  bucket = var.code_bucket_name
-  key    = "layer.zip"
-  source = "${path.module}/../layer.zip"
-  etag   = filemd5("${path.module}/../layer.zip")
-}
+# resource "aws_s3_object" "extract_extras_layer" {
+#   depends_on = [ aws_s3_bucket.code_bucket ]
+#   bucket = var.code_bucket_name
+#   key    = "extract_extras_layer.zip"
+#   source = "${path.module}/../extract_extras_layer.zip"
+#   etag   = filemd5("${path.module}/../extract_extras_layer.zip")
+# }
+
+# resource "aws_s3_object" "transform_extras_layer" {
+#   depends_on = [ aws_s3_bucket.code_bucket ]
+#   bucket = var.code_bucket_name
+#   key    = "transform_extras_layer.zip"
+#   source = "${path.module}/../transform_extras_layer.zip"
+#   etag   = filemd5("${path.module}/../transform_extras_layer.zip")
+# }
 
 resource "aws_s3_object" "utils" {
   depends_on = [ aws_s3_bucket.code_bucket ]
@@ -51,4 +59,12 @@ resource "aws_s3_bucket" "transformation_bucket" {
     Name        = "Data transformed"
     Environment = "prod" #both functions are in the same enviroment?
   }
+}
+
+resource "aws_s3_object" "transform_function" {
+  depends_on = [ aws_s3_bucket.code_bucket ]
+  bucket = var.code_bucket_name
+  key    = "transform_function.zip"
+  source = "${path.module}/../transform_function.zip"
+  etag   = filemd5(data.archive_file.transform_lambda.output_path)
 }
