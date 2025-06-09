d275b8c06112d7d59425d2eb6a7b2941ac225e23
SelvitaPR <selva.pla.roj@gmail.com>
2025-06-09 15:09:18 +0100

    add dependencies

--- make_zip.sh
+++ make_zip.sh
@@ -6,9 +6,19 @@ rm -rf python
 rm -rf util_layer.zip
 rm -rf layer.zip
 echo "Creating dependencies"
-pip install -r requirements.txt --platform manylinux2014_x86_64 --only-binary=:all: -t python/
+
+pip install python-dotenv pandas --platform manylinux2014_x86_64 --only-binary=:all: -t python/
 zip -r layer.zip python
 rm -rf python
+
+pip install psycopg2-binary pg8000 --platform manylinux2014_x86_64 --only-binary=:all: -t python/
+zip -r extract_extras_layer.zip python
+rm -rf python
+
+pip install pyarrow --platform manylinux2014_x86_64 --only-binary=:all: -t python/
+zip -r transform_extras_layer.zip python
+rm -rf python
+
 echo "Creating utils"
 mkdir python
 cp -r src python
