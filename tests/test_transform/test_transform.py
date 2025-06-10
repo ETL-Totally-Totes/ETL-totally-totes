658871a43332518e635f95332a60210317e3faa1
AbbyAdj <abbyadjei9@gmail.com>
2025-06-10 22:54:12 +0100

    almost finished tests

--- tests/test_transform/test_transform.py
+++ tests/test_transform/test_transform.py
@@ -15,7 +15,7 @@ from src.transform import (
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-
+@pytest.mark.skip
 class TestGetLogs:
     log_group_name = "test_log"
     log_group_stream = "test_stream"
@@ -43,12 +43,12 @@ class TestGetLogs:
         assert response == ["totally totes"]
 
     def test_handles_error(self, log_client, caplog):
-        get_logs(log_client, "log_group_name")
         with caplog.at_level(logging.INFO):
+            get_logs(log_client, "log_group_name")
             assert "an error occured with the log client" in caplog.text
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 class TestGetCSVFileKeys:
     def test_is_a_pure_function(self):
         test_logs = ["1", "two", "thr33"]
@@ -67,12 +67,14 @@ class TestGetCSVFileKeys:
         assert response == []
 
     def test_handles_error(self, caplog):
-        get_csv_file_keys(42)
         with caplog.at_level(logging.INFO):
-            assert "unexpected error occured" in caplog.text
+            get_csv_file_keys(42)
+        assert "unexpected error occured" in caplog.text
 
 
+# @pytest.mark.skip
 class TestTransformHandler:
+    @pytest.mark.skip
     def test_uploads_a_parquet_buffer_to_s3(
         self,
         s3_client,
@@ -85,70 +87,61 @@ class TestTransformHandler:
         # patch read_csv_to_df and create a new df for that return value
         transform_handler({"log_group_name": "test_log"}, None)
         response = s3_client.list_objects_v2(Bucket=TRANSFORM_BUCKET)
-        # pprint(response)
-        print(mock_read_csv_to_df)
-        # assert response["Contents"][0]["Key"][-8:] == ".parquet"
-        # assert response["KeyCount"] == 1
+        
+        assert response["Contents"][0]["Key"][-8:] == ".parquet"
+        assert response["KeyCount"] == 1
 
-    # def test_migrates_all_data_on_first_invocation(
-    #     self,
-    #     s3_client,
-    #     s3_with_transform_bucket,
-    #     mock_get_logs,
-    #     mock_get_csv_file_keys_v2,
-    #     mock_read_csv_to_df_v2,
-    # ):
-    #     transform_handler({"log_group_name": "test_log"}, None)
-    #     response = s3_client.list_objects_v2(Bucket=TRANSFORM_BUCKET)
-    #     assert response["KeyCount"] == 2
-    #     for file in response["Contents"]:
-    #         assert file["Key"][-8:] == ".parquet"
+    @pytest.mark.skip
+    def test_migrates_all_data_on_first_invocation(
+        self,
+        s3_client,
+        s3_with_transform_bucket,
+        mock_get_logs,
+        mock_get_csv_file_keys_v2,
+        mock_read_csv_to_df_v2,
+    ):
+        transform_handler({"log_group_name": "test_log"}, None)
+        response = s3_client.list_objects_v2(Bucket=TRANSFORM_BUCKET)
+        assert response["KeyCount"] == 2
+        for file in response["Contents"]:
+            assert file["Key"][-8:] == ".parquet"
 
-    # @pytest.mark.it(
-    #     "function migrates at least one table on subsequent invocations inclduing sales"
-    # )
-    # def test_subsequent_runs_inc_essentials(
-    #     self,
-    #     s3_client,
-    #     s3_with_transform_bucket,
-    #     mock_get_logs,
-    #     mock_get_csv_file_keys_v2,
-    #     mock_read_csv_to_df_v2
-    # ):
+    @pytest.mark.skip(
+        "function migrates at least one table on subsequent invocations inclduing sales"
+    )
+    def test_subsequent_runs_inc_essentials(
+        self,
+        s3_client,
+        s3_with_transform_bucket,
+        mock_get_logs,
+        mock_get_csv_file_keys_v2,
+        mock_read_csv_to_df_v2
+    ):
 
-    #     transform_handler({"log_group_name": "test_log"}, None)
+        transform_handler({"log_group_name": "test_log"}, None)
 
 
-    #     with patch("src.transform.get_csv_file_keys") as mock_keys, patch("src.transform.read_csv_to_df") as mock_csv_to_df:
-    #         test_dict = {
-    #         "index": [1],
-    #         "created_at": ["2025-06-04"],
-    #         "last_updated": ["2025-06-06"],
-    #         "result": ["it works"],
-    #         }
-    #         test_df = pd.DataFrame.from_dict(test_dict)
-    #         test_df.set_index("index", inplace=True)
-    #         mock_keys.return_value = ["2025/06/05/design.csv"]
-    #         mock_csv_to_df.return_value = {"2025/06/05/design.csv": test_df}
+        with patch("src.transform.get_csv_file_keys") as mock_keys, patch("src.transform.read_csv_to_df") as mock_csv_to_df:
+            mock_keys.return_value = ["2025/06/05/design.csv"]
 
-    #         transform_handler({"log_group_name": "test_log"}, None)
+            transform_handler({"log_group_name": "test_log"}, None)
 
-    #         response = s3_client.list_objects_v2(Bucket=TRANSFORM_BUCKET)
-    #         assert response["KeyCount"] == 3
-    #         for file in response["Contents"]:
-    #             assert file["Key"][-8:] == ".parquet"
+            response = s3_client.list_objects_v2(Bucket=TRANSFORM_BUCKET)
+            assert response["KeyCount"] == 3
+            for file in response["Contents"]:
+                assert file["Key"][-8:] == ".parquet"
 
     # @pytest.mark.xfail
-    # def test_subsequent_runs_no_changes(
-    #     self, caplog, mock_get_logs, mock_read_csv_to_df
-    # ):
-    #     # "function logs that there were no changes for an empty keys list"
-    #     # Should log this information
-    #     with patch("src.transform.get_csv_file_keys") as mock_keys:
-    #         caplog.set_level(logging.INFO)
-    #         mock_keys.return_value = []
-    #         transform_handler({"log_group_name": "test_log"}, None)
-    #         assert "no data was exported during this execution" in caplog.text
+    def test_subsequent_runs_no_changes(
+        self, caplog, mock_get_logs
+    ):
+        # "function logs that there were no changes for an empty keys list"
+        # Should log this information
+        with patch("src.transform.get_csv_file_keys") as mock_keys:
+            mock_keys.return_value = []
+            with caplog.at_level(logging.INFO, logger="src.transform"):
+                transform_handler({"log_group_name": "test_log"}, None)
+            assert "no data was exported during this execution" in caplog.text
 
     # @pytest.mark.xfail
     # def test_handles_sdk_client_error(
