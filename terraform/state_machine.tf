bef5d9b98a38da155b67271fa048e474fb6a71ed
AbbyAdj <abbyadjei9@gmail.com>
2025-06-10 22:53:24 +0100

    completed state machine

--- terraform/state_machine.tf
+++ terraform/state_machine.tf
@@ -1,20 +1,23 @@
-# resource "aws_sfn_state_machine" "etl_totally_totes_workflow" {
-#   name     = var.state_machine_name
-#   role_arn = aws_iam_role.state_role.arn
-#   publish  = true
-#   type     = "EXPRESS"
+resource "aws_sfn_state_machine" "etl_totally_totes_workflow" {
+  name     = var.state_machine_name
+  role_arn = aws_iam_role.state_role.arn
+  publish  = true
+  # logging_configuration {
+  #   include_execution_data = true
+  #   level = "ALL"
+  #   log_destination = "${aws_cloudwatch_log_group.etl_totally_totes_workflow_logs.arn}:*"
+  # }
+  type     = "STANDARD"
+  definition = templatefile("${path.module}/state-machine.asl.json", {
+    extract_arn = aws_lambda_function.extract_handler.arn,
+    transform_arn = aws_lambda_function.transform_handler.arn
+    # load_arn = aws_lambda_function.load_handler.arn,
+    }
+  )
 
-#   definition = ##THIS WHOLE BLOCK WILL BE DEFINED AFTER WE USED STEP FUNCTIONS
-# {
-#   "Comment": "A Hello World example of the Amazon States Language using an AWS Lambda Function",
-#   "StartAt": "HelloWorld",
-#   "States": {
-#     "HelloWorld": {
-#       "Type": "Task",
-#       "Resource": "${aws_lambda_function.lambda.arn}",
-#       "End": true
-#     }
-#   }
-# }
+}
 
-# }
+
+resource "aws_cloudwatch_log_group" "etl_totally_totes_workflow_logs" {
+  name = "etl_totally_totes_workflow_logs"
+}
