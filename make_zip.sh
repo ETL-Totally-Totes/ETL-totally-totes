671fab30919dd46fec7e232716c1f2b3c4cbc5a9
AbbyAdj <abbyadjei9@gmail.com>
2025-06-10 22:54:40 +0100

    removed a command block

--- make_zip.sh
+++ make_zip.sh
@@ -7,16 +7,13 @@ rm -rf util_layer.zip
 rm -rf layer.zip
 echo "Creating dependencies"
 
-pip install python-dotenv pandas --platform manylinux2014_x86_64 --only-binary=:all: -t python/
+pip install python-dotenv --platform manylinux2014_x86_64 --only-binary=:all: -t python/
 zip -r layer.zip python
 rm -rf python
 
-pip install psycopg2-binary pg8000 --platform manylinux2014_x86_64 --only-binary=:all: -t python/
+# pip uninstall pyscopg2
+pip install psycopg2-binary==2.9.9 --platform manylinux2014_x86_64 --only-binary=:all: -t python/
 zip -r extract_extras_layer.zip python
-rm -rf python
-
-pip install pyarrow --platform manylinux2014_x86_64 --only-binary=:all: -t python/
-zip -r transform_extras_layer.zip python
 rm -rf python
 
 echo "Creating utils"
